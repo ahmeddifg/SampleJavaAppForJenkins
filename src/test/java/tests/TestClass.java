aa5d902018ff40918b57b25059ce0382eb69991d
Ahmed Dif <ahmed.def.g@gmail.com>
2019-11-24 22:28:50 +0300

    done

---
+++ src/test/java/tests/TestClass.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestClass {
+    @Test
+    public void test() {
+    assertEquals(5,5);
+    }
+}
