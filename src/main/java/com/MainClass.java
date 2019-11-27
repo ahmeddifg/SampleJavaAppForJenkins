596da80c94c900ad4fd712a9663d19f745b50ffd
Ahmed Dif <ahmed.def.g@gmail.com>
2019-11-27 11:10:37 +0300

    done

--- src/main/java/com/MainClass.java
+++ src/main/java/com/MainClass.java
@@ -3,6 +3,6 @@ package com;
 public class MainClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello there00...");
+        System.out.println("Hello there...");
     }
 }
